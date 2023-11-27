2132077a8ffab8e656d472f43fb0de92dcd16a10
Anderpros <95669361+Anderpros@users.noreply.github.com>
2023-11-27 20:27:08 +0700

    Update tree.h, add some comments

--- tree.h
+++ tree.h
@@ -7,12 +7,14 @@
 
 using namespace std;
 
+//Class representing a node in the binary search tree
 class TreeNode {
 public:
     Movie data;
     TreeNode* left;
     TreeNode* right;
 
+    //Constructor to initialize tree node with movie data
     TreeNode(const Movie& m) : data(m), left(nullptr), right(nullptr) {}
 };
 
